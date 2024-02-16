5d3a26ab91c58e7142d94a6bbc624ce484af54db
sm.pereverzev <sm.pereverzev@severstal.com>
2024-02-16 12:32:49 +0300

    +

--- src/main/java/foo/bar/rest/RequestsManagerNetty.java
+++ src/main/java/foo/bar/rest/RequestsManagerNetty.java
@@ -5,7 +5,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Flux;
-import reactor.core.publisher.Mono;
 import reactor.core.scheduler.Scheduler;
 import reactor.core.scheduler.Schedulers;
 
